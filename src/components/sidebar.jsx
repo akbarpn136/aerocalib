fa7ba7409d9c6df076f5db5cd9aa160f911723c5
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-02-12 13:29:57 +0700

    Pembuatan komponen awal sidebar

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,22 @@
+import { For } from "solid-js"
+
+export default function SimpleSidebar() {
+    const tmp = [...Array(10).keys()]
+
+    return <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
+        <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+            <ul class="space-y-2 font-medium">
+                <For each={tmp} fallback={<p>Loading...</p>}>
+                    {item => <li>
+                        <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7H5a2 2 0 0 0-2 2v4m5-6h8M8 7V5c0-1.1.9-2 2-2h4a2 2 0 0 1 2 2v2m0 0h3a2 2 0 0 1 2 2v4m0 0v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-6m18 0s-4 2-9 2-9-2-9-2m9-2h0" />
+                            </svg>
+                            <span class="ml-3">Peralatan PT. ABC {item + 1}</span>
+                        </a>
+                    </li>}
+                </For>
+            </ul>
+        </div>
+    </aside>
+}
