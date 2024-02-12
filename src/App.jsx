afb44bc00f1b2b119cdd58e24d0ca6069fc9c72e
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-02-12 13:29:13 +0700

    Menerapkan komponen utama

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import "flowbite"
+
+import SimpleNavbar from "./components/navbar"
+import SimpleSidebar from "./components/sidebar"
+
 function App() {
-    return (
-        <div class="text-3xl font-bold underline">
-            <h1>Halaman Utama AeroCalib</h1>
-        </div>
-    );
+    return <>
+        <SimpleNavbar />
+
+        <SimpleSidebar />
+
+        <div class="p-4 sm:ml-64">
+            <div class="p-4 mt-14">
+                <p>INI HALAMAN UTAMA.</p>
+            </div>
+        </div >
+    </>
 }
 
-export default App;
+export default App
