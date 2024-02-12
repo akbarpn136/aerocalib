76142918be83cdbd9f0ee32e9e10cdb7d4b96629
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-02-12 11:55:37 +0700

    Penyesuian struktur sesuai komponen UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
     return (
-        <div class="container">
+        <div class="text-3xl font-bold underline">
             <h1>Halaman Utama AeroCalib</h1>
         </div>
     );
