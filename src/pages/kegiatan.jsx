ea720956b2ef86356689ee6c752a2b3685b06a33
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-02-13 12:58:06 +0700

    Membuat halaman awal utama

---
+++ src/pages/kegiatan.jsx
@@ -0,0 +1,7 @@
+export default function HalamanUtama() {
+    return <div class="p-4 sm:ml-64">
+        <div class="p-4 mt-14">
+            <p>INI HALAMAN KEGIATAN.</p>
+        </div>
+    </div >
+}
