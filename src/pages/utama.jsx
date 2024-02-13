d1119279112b7bf69c7a093c6fcbcd356724f3d2
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-02-13 12:58:20 +0700

    Membuat halaman utama

---
+++ src/pages/utama.jsx
@@ -0,0 +1,7 @@
+export default function HalamanUtama() {
+    return <div class="p-4 sm:ml-64">
+        <div class="p-4 mt-14">
+            <p>INI HALAMAN UTAMA.</p>
+        </div>
+    </div >
+}
