bdf689525f67be10121ad85c7dd5f4c0fc4b1f4f
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-02-15 13:24:08 +0700

    Membuat halaman awal kegiatan rinci

---
+++ src/pages/kegiatan/rincian.jsx
@@ -0,0 +1,7 @@
+export default function RincianKegiatan() {
+    return <div class="p-4 sm:ml-64">
+        <div class="p-4 mt-14">
+            <p>INI HALAMAN RINCIAN KEGIATAN.</p>
+        </div>
+    </div >
+}
