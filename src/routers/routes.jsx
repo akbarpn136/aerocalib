1efc8f5b9ee0305a782a0898ce9c3c707bed27eb
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-02-15 13:24:25 +0700

    Daftar route baru

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -3,7 +3,12 @@ import { lazy } from "solid-js"
 export const routes = [
     {
         path: "/kegiatan",
-        component: lazy(() => import("../pages/kegiatan.jsx")),
+        children: [
+            {
+                path: "/:id",
+                component: lazy(() => import("../pages/kegiatan/rincian.jsx")),
+            },
+        ]
     },
     {
         path: "/",
