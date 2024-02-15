484a37fe2cd378c050edb952ea0ec28b493f513a
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-02-15 13:22:08 +0700

    Hapus halaman kegiatan yang tidak digunakan

--- src/pages/kegiatan.jsx
+++
@@ -1,7 +0,0 @@
-export default function HalamanUtama() {
-    return <div class="p-4 sm:ml-64">
-        <div class="p-4 mt-14">
-            <p>INI HALAMAN KEGIATAN.</p>
-        </div>
-    </div >
-}
