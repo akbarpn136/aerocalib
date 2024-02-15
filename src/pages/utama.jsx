a45b8168d933561ecdfca21f52a72b7ffee3456b
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-02-15 13:23:45 +0700

    Modifikasi halaman utama

--- src/pages/utama.jsx
+++ src/pages/utama.jsx
@@ -1,7 +1,7 @@
-import Info from "../components/info"
+import { InfoUmum } from "../components/info"
 
 export default function HalamanUtama() {
     return <div class="sm:ml-64">
-        <Info />
-    </div >
+        <InfoUmum />
+    </div>
 }
