cc78a6f6fbb5bf07faed4cbe36b05b43642d202a
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-02-15 13:22:23 +0700

    Modifikasi layout utama

--- src/layouts/default.jsx
+++ src/layouts/default.jsx
@@ -9,7 +9,10 @@ function DefaultLayout(props) {
 
         <SimpleSidebar />
 
-        {props.children}
+        <section class="bg-white dark:bg-gray-900 bg-[url('lightpattern.svg')] dark:bg-[url('darkpattern.svg')]">
+            {props.children}
+            <div class="bg-gradient-to-t from-gray-100 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0"></div>
+        </section>
     </>
 }
 
