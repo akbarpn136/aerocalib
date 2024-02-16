283ea92d7d53e0af0fbd8b102bc783ebcaf1894d
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-02-16 16:46:06 +0700

    Atur theme awal

--- src/layouts/default.jsx
+++ src/layouts/default.jsx
@@ -1,9 +1,16 @@
 import "flowbite"
+import { onMount } from "solid-js"
 
 import SimpleNavbar from "../components/navbar"
 import SimpleSidebar from "../components/sidebar"
 
 function DefaultLayout(props) {
+    onMount(() => {
+        if (!localStorage.getItem("color-theme")) {
+            localStorage.setItem("color-theme", "light");
+        }
+    })
+
     return <>
         <SimpleNavbar />
 
@@ -11,7 +18,7 @@ function DefaultLayout(props) {
 
         <section class="bg-white dark:bg-gray-900 bg-[url('lightpattern.svg')] dark:bg-[url('darkpattern.svg')]">
             {props.children}
-            <div class="bg-gradient-to-t from-gray-100 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0"></div>
+            <div class="bg-gradient-to-t from-gray-100 to-transparent dark:from-gray-900 w-full h-full absolute top-0 left-0 z-0"></div>
         </section>
     </>
 }
