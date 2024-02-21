75fb57a94160e49ee484cd34c6d2ad5bd71b66f3
Fulan Gulan <fulan.gulan@gmail.com>
2024-02-21 13:24:34 +0700

    Pembuatan awal store utama

---
+++ src/stores/utama.jsx
@@ -0,0 +1,12 @@
+import { createContext } from "solid-js"
+import { createClient } from "@supabase/supabase-js"
+
+export const ContextUtama = createContext()
+
+export const StateUtama = {
+    db: createClient(
+        import.meta.env.VITE_SUPABASE_URL,
+        import.meta.env.VITE_SUPABASE_KEY,
+        { db: { schema: "aerocalib" } }
+    ),
+}
