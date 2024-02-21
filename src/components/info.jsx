837c5214173a1720b8329721d8b8e2a84d622308
Fulan Gulan <fulan.gulan@gmail.com>
2024-02-21 13:21:21 +0700

    Tambah komponen untuk informasi error

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -19,7 +19,7 @@ export function InfoUmum() {
             </h1>
             <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">
                 Silahkan pilih daftar kegiatan yang ditampilkan pada menu samping untuk melihat
-                rincian dari masng-masing kegiatan kalibrasi...🙏🏼
+                rincian dari masing-masing kegiatan kalibrasi...🙏🏼
             </p>
         </div>
     </div>
@@ -46,6 +46,21 @@ export function Info404() {
                 Maaf, halaman yang Anda cari tidak ditemukan. Untuk itu silahkan kembali ke halaman
                 utama melalui tautan di atas.
             </p>
+        </div>
+    </div>
+}
+
+export function InfoErrorUmum(props) {
+    return <div class="p-4 mb-4 text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
+        <div class="flex items-center">
+            <svg class="flex-shrink-0 w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+            </svg>
+            <span class="sr-only">Info</span>
+            <h3 class="text-lg font-medium">Terjadi kesalahan</h3>
+        </div>
+        <div class="mt-2 mb-4 text-sm">
+            {props.pesan}
         </div>
     </div>
 }
