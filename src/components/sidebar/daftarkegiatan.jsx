692c61fdcaf70528882c9b595caab91d984e35d5
Fulan Gulan <fulan.gulan@gmail.com>
2024-02-21 14:25:34 +0700

    Mulai penerapan state kegiatan via resource

--- src/components/sidebar/daftarkegiatan.jsx
+++ src/components/sidebar/daftarkegiatan.jsx
@@ -1,35 +1,46 @@
-import { For, useContext, createResource } from "solid-js"
+import { For, Show, onMount, useContext, createResource } from "solid-js"
 import { A } from "@solidjs/router"
 
 import SkeletonKegiatan from "./skeletonkegiatan"
 import { ContextUtama } from "../../stores/utama"
 
 export default function DaftarKegiatan() {
-    const { state } = useContext(ContextUtama)
+    const { state, setState } = useContext(ContextUtama)
 
     const getKegiatan = async () => {
         try {
-            const { data } = await state.db.from("kegiatan").select("id, nama")
+            const { data } = await state.db
+                .from("kegiatan")
+                .select("id, nama")
+                .limit(10)
+                .order("created_at", { ascending: false })
 
-            return data
+            data.map(kegiatan => {
+                setState("kegiatan", [...state.kegiatan, kegiatan])
+            })
         } catch (err) {
             throw new Error(err)
         }
     }
 
-    const [kegiatan] = createResource(getKegiatan)
+    onMount(() => {
+        setState("kegiatan", [])
+        createResource(getKegiatan)
+    })
 
-    return <ul class="space-y-2 font-medium">
-        <For each={kegiatan()} fallback={<SkeletonKegiatan />}>
-            {item => <li>
-                <A activeClass="link-active"
-                    href={`/kegiatan/${item.id}`} class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7H5a2 2 0 0 0-2 2v4m5-6h8M8 7V5c0-1.1.9-2 2-2h4a2 2 0 0 1 2 2v2m0 0h3a2 2 0 0 1 2 2v4m0 0v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-6m18 0s-4 2-9 2-9-2-9-2m9-2h0" />
-                    </svg>
-                    <span class="ml-3">{item.nama}</span>
-                </A>
-            </li>}
-        </For>
-    </ul>
+    return <Show when={state.kegiatan.length != 0} fallback={<SkeletonKegiatan />}>
+        <ul class="space-y-2 font-medium">
+            <For each={state.kegiatan}>
+                {item => <li>
+                    <A activeClass="link-active"
+                        href={`/kegiatan/${item.id}`} class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7H5a2 2 0 0 0-2 2v4m5-6h8M8 7V5c0-1.1.9-2 2-2h4a2 2 0 0 1 2 2v2m0 0h3a2 2 0 0 1 2 2v4m0 0v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-6m18 0s-4 2-9 2-9-2-9-2m9-2h0" />
+                        </svg>
+                        <span class="ml-3">{item.nama}</span>
+                    </A>
+                </li>}
+            </For>
+        </ul>
+    </Show>
 }
