511b5097385974522c8f4834f62e74c85b51a632
Fulan Gulan <fulan.gulan@gmail.com>
2024-02-21 13:24:07 +0700

    Registrasi awal state

--- src/layouts/default.jsx
+++ src/layouts/default.jsx
@@ -1,18 +1,21 @@
 import "flowbite"
 import { onMount } from "solid-js"
+import { createStore } from "solid-js/store"
 
 import SimpleNavbar from "../components/navbar"
 import SimpleSidebar from "../components/sidebar"
+import { StateUtama, ContextUtama } from "../stores/utama"
 
 function DefaultLayout(props) {
+    const [state, setState] = createStore(StateUtama)
+
     onMount(() => {
-        console.log(import.meta.env.VITE_SUPABASE_URL)
         if (!localStorage.getItem("color-theme")) {
             localStorage.setItem("color-theme", "light");
         }
     })
 
-    return <>
+    return <ContextUtama.Provider value={{ state, setState }}>
         <SimpleNavbar />
 
         <SimpleSidebar />
@@ -21,7 +24,7 @@ function DefaultLayout(props) {
             {props.children}
             <div class="bg-gradient-to-t from-gray-100 to-transparent dark:from-gray-900 w-full h-full absolute top-0 left-0 z-0"></div>
         </section>
-    </>
+    </ContextUtama.Provider>
 }
 
 export default DefaultLayout
