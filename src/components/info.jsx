4f22003a89bd5ff6c6db998a49261b0ed2e4b45c
Fulan Gulan <fulan.gulan@gmail.com>
2024-02-21 14:24:24 +0700

    Membuat komponen info box

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -50,7 +50,7 @@ export function Info404() {
     </div>
 }
 
-export function InfoErrorUmum(props) {
+export function InfoErrorBox(props) {
     return <div class="p-4 mb-4 text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
         <div class="flex items-center">
             <svg class="flex-shrink-0 w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -60,6 +60,23 @@ export function InfoErrorUmum(props) {
             <h3 class="text-lg font-medium">Terjadi kesalahan</h3>
         </div>
         <div class="mt-2 mb-4 text-sm">
+            {props.pesan}
+        </div>
+    </div>
+}
+
+export function InfoUmumBox(props) {
+    return <div id="alert-additional-content-5" class="p-4 border border-gray-300 rounded-lg bg-gray-50 dark:border-gray-600 dark:bg-gray-800" role="alert">
+        <div class="flex items-center">
+            <svg class="flex-shrink-0 w-4 h-4 me-2 dark:text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+            </svg>
+            <span class="sr-only">Info</span>
+            <h3 class="text-lg font-medium text-gray-800 dark:text-gray-300">
+                Pesan singkat
+            </h3>
+        </div>
+        <div class="mt-2 mb-4 text-sm text-gray-800 dark:text-gray-300">
             {props.pesan}
         </div>
     </div>
