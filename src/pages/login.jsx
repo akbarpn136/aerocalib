dce5d34fd8a25c6f35517d78f2a1af6b52dc23d7
Fulan Gulan <fulan.gulan@gmail.com>
2024-02-26 12:58:10 +0700

    Membuat halaman baru untuk login

---
+++ src/pages/login.jsx
@@ -0,0 +1,7 @@
+export default function HalamanUtama() {
+    return <div class="h-screen p-4 sm:ml-64">
+        <div class="p-4 mt-14">
+            <p class="text-gray-500 dark:text-gray-200">INI HALAMAN LOGIN.</p>
+        </div>
+    </div >
+}
