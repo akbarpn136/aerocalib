865272cd5b0891efb3459f06a4fc102839c51a29
Fulan Gulan <fulan.gulan@gmail.com>
2024-02-26 12:56:37 +0700

    Modifikasi format router

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,23 @@
 /* @refresh reload */
+import { lazy } from "solid-js"
 import { render } from "solid-js/web"
-import { Router } from "@solidjs/router"
+import { Router, Route } from "@solidjs/router"
+import { createStore } from "solid-js/store"
 
 import "./styles.css"
+import RouterKegiatan from "./routers/kegiatan"
 import DefaultLayout from "./layouts/default"
-import { routes } from "./routers/routes"
+import { StateUtama, ContextUtama } from "./stores/utama"
 
-render(() => <Router root={DefaultLayout}>{routes}</Router>, document.getElementById("root"))
+render(() => {
+    const [state, setState] = createStore(StateUtama)
+
+    return <ContextUtama.Provider value={{ state, setState }}>
+        <Router root={DefaultLayout}>
+            <RouterKegiatan />
+            <Route path="/login" component={lazy(() => import("./pages/login.jsx"))} />
+            <Route path="/" component={lazy(() => import("./pages/utama.jsx"))} />
+            <Route path="*404" component={lazy(() => import("./pages/404.jsx"))} />
+        </Router>
+    </ContextUtama.Provider>
+}, document.getElementById("root"))
