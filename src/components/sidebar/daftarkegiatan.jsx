3ec81b11c71a9e4f8fec219227a53198debf4698
Fulan Gulan <fulan.gulan@gmail.com>
2024-02-26 09:48:04 +0700

    Modifikasi kondisi kegiatan

--- src/components/sidebar/daftarkegiatan.jsx
+++ src/components/sidebar/daftarkegiatan.jsx
@@ -1,7 +1,8 @@
 import { A } from "@solidjs/router"
-import { For, Show, onMount, useContext, createSignal, createResource } from "solid-js"
+import { For, Show, Switch, Match, onMount, useContext, createSignal, createResource } from "solid-js"
 
 import SkeletonKegiatan from "./skeletonkegiatan"
+import { InfoUmumBox } from "../../components/info"
 import { ContextUtama } from "../../stores/utama"
 
 export default function DaftarKegiatan() {
@@ -9,22 +10,6 @@ export default function DaftarKegiatan() {
     const [hiddenMuat, setHiddenMuat] = createSignal(false)
     const [pageKegiatan, setPageKegiatan] = createSignal(1)
     const [loadingMuat, setLoadingMuat] = createSignal(false)
-
-    const getKegiatan = async () => {
-        try {
-            const { data } = await state.db
-                .from("kegiatan")
-                .select("id, nama")
-                .limit(import.meta.env.VITE_LIMIT_KEGIATAN)
-                .order("created_at", { ascending: false })
-
-            data.map(kegiatan => {
-                setState("kegiatan", [...state.kegiatan, kegiatan])
-            })
-        } catch (err) {
-            throw new Error(err)
-        }
-    }
 
     const onPageKegiatan = async () => {
         setPageKegiatan(pageKegiatan() + 1)
@@ -55,42 +40,53 @@ export default function DaftarKegiatan() {
         }
     }
 
+    const [keg] = createResource(onPageKegiatan)
+
     onMount(() => {
         setState("kegiatan", [])
-        createResource(onPageKegiatan)
     })
 
-    return <Show when={state.kegiatan.length != 0} fallback={<SkeletonKegiatan />}>
-        <ul class="space-y-2 font-medium">
-            <For each={state.kegiatan}>
-                {item => <li>
-                    <A activeClass="link-active"
-                        href={`/kegiatan/${item.id}`} class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                        <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7H5a2 2 0 0 0-2 2v4m5-6h8M8 7V5c0-1.1.9-2 2-2h4a2 2 0 0 1 2 2v2m0 0h3a2 2 0 0 1 2 2v4m0 0v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-6m18 0s-4 2-9 2-9-2-9-2m9-2h0" />
-                        </svg>
-                        <span class="ml-3">{item.nama}</span>
-                    </A>
-                </li>}
-            </For>
-        </ul>
+    return <Switch>
+        <Match when={keg.loading}>
+            <SkeletonKegiatan />
+        </Match>
 
-        <Show when={!hiddenMuat()}>
-            <div class="my-4">
-                <button
-                    type="button"
-                    class="w-full py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                    onClick={onPageKegiatan}
-                >
-                    <Show when={loadingMuat()}>
-                        <svg aria-hidden="true" role="status" class="inline w-4 h-4 me-3 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2" />
-                        </svg>
-                    </Show>
-                    Muat Kegiatan
-                </button>
-            </div>
-        </Show>
-    </Show>
+        <Match when={state.kegiatan.length == 0}>
+            <InfoUmumBox pesan="Kegiatan tidak ditemukan." />
+        </Match>
+
+        <Match when={state.kegiatan.length > 0}>
+            <ul class="space-y-2 font-medium">
+                <For each={state.kegiatan}>
+                    {item => <li>
+                        <A activeClass="link-active"
+                            href={`/kegiatan/${item.id}`} class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7H5a2 2 0 0 0-2 2v4m5-6h8M8 7V5c0-1.1.9-2 2-2h4a2 2 0 0 1 2 2v2m0 0h3a2 2 0 0 1 2 2v4m0 0v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-6m18 0s-4 2-9 2-9-2-9-2m9-2h0" />
+                            </svg>
+                            <span class="ml-3">{item.nama}</span>
+                        </A>
+                    </li>}
+                </For>
+            </ul>
+
+            <Show when={!hiddenMuat()}>
+                <div class="my-4">
+                    <button
+                        type="button"
+                        class="w-full py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                        onClick={onPageKegiatan}
+                    >
+                        <Show when={loadingMuat()}>
+                            <svg aria-hidden="true" role="status" class="inline w-4 h-4 me-3 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2" />
+                            </svg>
+                        </Show>
+                        Muat Kegiatan
+                    </button>
+                </div>
+            </Show>
+        </Match>
+    </Switch>
 }
