d2e4708b6725842707817429302ea97926b2f0e6
Fulan Gulan <fulan.gulan@gmail.com>
2024-02-27 11:16:49 +0700

    Pengaturan ulang tata letak

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,8 @@
 import { Info404 } from "../components/info"
 
 export default function HalamanUtama() {
-    return <div class="sm:ml-64">
+    return <section class="bg-white dark:bg-gray-900 bg-[url('lightpattern.svg')] dark:bg-[url('darkpattern.svg')]">
         <Info404 />
-    </div>
+        <div class="bg-gradient-to-t from-gray-100 to-transparent dark:from-gray-900 w-full h-full absolute top-0 left-0 z-0"></div>
+    </section>
 }
