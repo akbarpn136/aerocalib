9a929e62cf23bc219108c53cb12b8aea65fd0cfd
Fulan Gulan <fulan.gulan@gmail.com>
2024-02-29 13:20:39 +0700

    Lanjut kelola mekanisme login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,4 +1,4 @@
-import { createSignal, useContext, onMount } from "solid-js"
+import { createSignal, useContext, onMount, Show } from "solid-js"
 import { useNavigate } from "@solidjs/router"
 
 import { ContextUtama } from "../stores/utama"
@@ -8,6 +8,8 @@ export default function HalamanLogin() {
     const goto = useNavigate()
     const [email, setEmail] = createSignal("")
     const [sandi, setSandi] = createSignal("")
+    const [pesan, setPesan] = createSignal("")
+    const [lihat, setLihat] = createSignal(false)
     const { state } = useContext(ContextUtama)
 
     onMount(async () => {
@@ -20,13 +22,20 @@ export default function HalamanLogin() {
 
     const onFormKegiatanSubmit = async (e) => {
         e.preventDefault()
+        setLihat(false)
 
-        let { data, error } = await state.supa.auth.signInWithPassword({
+        let { error } = await state.supa.auth.signInWithPassword({
             email: email(),
             password: sandi()
         })
 
-        console.log(data)
+        if (error) {
+            setPesan(error.message)
+            setLihat(true)
+        } else {
+            setLihat(false)
+            goto("/", { replace: true })
+        }
     }
 
     return <div class="m-auto w-full md:w-96 p-2 z-10 relative">
@@ -69,22 +78,24 @@ export default function HalamanLogin() {
             </form>
         </div>
 
-        <div id="toast-warning" class="fixed bottom-5 right-5 flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
-            <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg dark:bg-orange-700 dark:text-orange-200">
-                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z" />
-                </svg>
-                <span class="sr-only">Warning icon</span>
+        <Show when={lihat()}>
+            <div id="toast-warning" class="fixed bottom-5 right-5 flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+                <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg dark:bg-orange-700 dark:text-orange-200">
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z" />
+                    </svg>
+                    <span class="sr-only">Warning icon</span>
+                </div>
+                <div class="ms-3 text-sm font-normal">{pesan()}</div>
+                <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+                    onClick={() => setLihat(false)}
+                    aria-label="Close">
+                    <span class="sr-only">Close</span>
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                    </svg>
+                </button>
             </div>
-            <div class="ms-3 text-sm font-normal">Improve password difficulty.</div>
-            <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-                data-dismiss-target="#toast-warning"
-                aria-label="Close">
-                <span class="sr-only">Close</span>
-                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                </svg>
-            </button>
-        </div>
+        </Show>
     </div>
 }
