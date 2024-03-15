41cb913d76bed51e08d51d354137c29f1cb3ae66
Fulan Gulan <fulan.gulan@gmail.com>
2024-03-15 09:35:13 +0700

    Penyesuaian visibilitas berdasarkan ukuran media

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -3,13 +3,13 @@ import { A } from "@solidjs/router"
 export function InfoUmum() {
     return <div class="flex h-screen">
         <div class="py-8 px-4 m-auto max-w-screen-xl text-center lg:py-16 z-10 relative">
-            <A href="/info" class="mt-14 inline-flex justify-between items-center py-1 px-1 pe-4 mb-7 text-sm text-blue-700 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800">
-                <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 me-3">
+            <A href="/info" class="mt-14 inline-flex justify-between items-center p-1 mb-7 text-sm text-blue-700 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800">
+                <span class="hidden sm:flex text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 me-3">
                     <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11h2v5m-2 0h4m-2.6-8.5h0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>
                 </span>
-                <span class="text-sm font-medium">Yuk mampir, untuk mengenal tentang aplikasi ini</span>
+                <span class="text-sm font-medium p-2 sm:p-0">Yuk mampir, untuk mengenal tentang aplikasi ini</span>
                 <svg class="w-2.5 h-2.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
                 </svg>
