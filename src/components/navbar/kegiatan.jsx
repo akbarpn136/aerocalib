706bc8ced1e8de2670a260aee860ce2787163cce
Fulan Gulan <fulan.gulan@gmail.com>
2024-03-18 13:32:51 +0700

    Membuat interaksi awal form kegiatan

--- src/components/navbar/kegiatan.jsx
+++ src/components/navbar/kegiatan.jsx
@@ -1,6 +1,44 @@
 import "flowbite"
+import { Modal } from "flowbite"
+import { createSignal, useContext } from "solid-js"
+
+import { ContextUtama } from "../../stores/utama"
 
 export default function TambahKegiatan() {
+    const { state, setState } = useContext(ContextUtama)
+    const [statusErr, setStatusErr] = createSignal(false)
+    const [pesanErr, setPesanErr] = createSignal("")
+    const [nama, setNama] = createSignal("")
+    const [deskripsi, setDeskripsi] = createSignal("")
+
+    const onFormKegiatanSubmit = async (e) => {
+        e.preventDefault()
+
+        const now = new Date().toLocaleString("sv-SE")
+        const { data, error } = await state.supa
+            .from("kegiatan")
+            .insert({
+                nama: nama(),
+                deskripsi: deskripsi(),
+                created_at: now
+            })
+            .select()
+
+        if (error) {
+            setStatusErr(true)
+            setPesanErr(error.message)
+        } else {
+            const targetEl = document.getElementById("modal-kegiatan")
+            const modal = new Modal(targetEl)
+
+            modal.hide()
+
+            setNama("")
+            setDeskripsi("")
+        }
+
+    }
+
     return <>
         <button
             type="button"
@@ -19,7 +57,7 @@ export default function TambahKegiatan() {
                 <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                     <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                         <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                            Sign in to our platform
+                            Kelola kegiatan
                         </h3>
                         <button type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="modal-kegiatan">
                             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -30,28 +68,32 @@ export default function TambahKegiatan() {
                     </div>
 
                     <div class="p-4 md:p-5">
-                        <form class="space-y-4" action="#">
+                        <form class="space-y-4" onSubmit={onFormKegiatanSubmit}>
                             <div>
-                                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
+                                <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama kegiatan</label>
+                                <input
+                                    type="text"
+                                    name="nama"
+                                    id="nama"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Nama mitra kerja" required
+                                    value={nama()}
+                                    onChange={e => setNama(e.currentTarget.value)}
+                                />
                             </div>
                             <div>
-                                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                                <label for="deskripsi" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Deskripsi (opsional)</label>
+                                <textarea
+                                    id="message"
+                                    rows="2"
+                                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Keterangan mengenai mitra..."
+                                    nama={deskripsi()}
+                                    onChange={e => setDeskripsi(e.currentTarget.value)}
+                                ></textarea>
                             </div>
-                            <div class="flex justify-between">
-                                <div class="flex items-start">
-                                    <div class="flex items-center h-5">
-                                        <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
-                                    </div>
-                                    <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-                                </div>
-                                <a href="#" class="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
-                            </div>
-                            <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
-                            <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-                                Not registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
-                            </div>
+
+                            <button type="submit" class="uppercase w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Simpan
+                            </button>
                         </form>
                     </div>
                 </div>
