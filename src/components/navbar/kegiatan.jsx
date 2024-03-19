534dde9c17ad849c7c9fdc5c9fca68c3a2ed855b
Fulan Gulan <fulan.gulan@gmail.com>
2024-03-19 12:06:04 +0700

    Modifikasi lanjut penambahan kegiatan

--- src/components/navbar/kegiatan.jsx
+++ src/components/navbar/kegiatan.jsx
@@ -1,6 +1,5 @@
-import "flowbite"
 import { Modal } from "flowbite"
-import { createSignal, useContext } from "solid-js"
+import { createSignal, useContext, onMount } from "solid-js"
 
 import { ContextUtama } from "../../stores/utama"
 
@@ -10,6 +9,19 @@ export default function TambahKegiatan() {
     const [pesanErr, setPesanErr] = createSignal("")
     const [nama, setNama] = createSignal("")
     const [deskripsi, setDeskripsi] = createSignal("")
+
+    let refModal
+    const onModalKegiatanShow = () => {
+        const modal = new Modal(refModal)
+
+        modal.show()
+    }
+
+    const onModalKegiatanHide = () => {
+        const modal = new Modal(refModal)
+
+        modal.hide()
+    }
 
     const onFormKegiatanSubmit = async (e) => {
         e.preventDefault()
@@ -28,10 +40,10 @@ export default function TambahKegiatan() {
             setStatusErr(true)
             setPesanErr(error.message)
         } else {
-            const targetEl = document.getElementById("modal-kegiatan")
-            const modal = new Modal(targetEl)
+            const kegiatan = data[0]
 
-            modal.hide()
+            onModalKegiatanHide()
+            setState("kegiatan", [kegiatan, ...state.kegiatan])
 
             setNama("")
             setDeskripsi("")
@@ -42,9 +54,8 @@ export default function TambahKegiatan() {
     return <>
         <button
             type="button"
-            data-modal-target="modal-kegiatan"
-            data-modal-toggle="modal-kegiatan"
             class="border border-gray-200 dark:border-gray-500 text-gray-700  bg-white dark:bg-gray-800 hover:bg-gray-100 hover:text-blue-700 focus:ring-2 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-blue-700 dark:focus:ring-gray-500"
+            onClick={onModalKegiatanShow}
         >
             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5" />
@@ -52,14 +63,19 @@ export default function TambahKegiatan() {
             <span class="sr-only">Tambah kegiatan</span>
         </button>
 
-        <div id="modal-kegiatan" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div id="modal-kegiatan" ref={refModal} tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
             <div class="relative p-4 w-full max-w-md max-h-full">
                 <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                     <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                         <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
                             Kelola kegiatan
                         </h3>
-                        <button type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="modal-kegiatan">
+                        <button
+                            type="button"
+                            class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                            onClick={onModalKegiatanHide}
+                            data-modal-hide="modal-kegiatan"
+                        >
                             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                             </svg>
