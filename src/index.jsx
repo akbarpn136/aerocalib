06ea5940be3444ec810a067d9b5a57e68eb3ab1a
Fulan Gulan <fulan.gulan@gmail.com>
2024-08-19 13:30:06 +0700

    Penambahan style

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 /* @refresh reload */
 import { render } from "solid-js/web"
 
+import "./index.css"
 import Default from "./layouts/default"
 
 render(() => <Default />, document.getElementById("root"));
