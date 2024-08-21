1a0fe0242aa7db44ca2297b833cbfaff2f2c3c75
Fulan Gulan <fulan.gulan@gmail.com>
2024-08-21 13:21:32 +0700

    Mendaftar route baru untuk rincian kegiatan

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -8,10 +8,12 @@ import HalamanKosong from "../pages/404"
 export default function MainRoutes() {
     const Arsip = lazy(() => import("../pages/arsip"))
     const Tentang = lazy(() => import("../pages/tentang"))
+    const Rincian = lazy(() => import("../pages/rincian"))
 
     return <Router>
         <Route path="/" component={Default}>
             <Route path="/" component={HalamanUtama} />
+            <Route path="/:id/rincian" component={<Rincian />} />
             <Route path="/arsip" component={<Arsip />} />
             <Route path="/tentang" component={<Tentang />} />
         </Route>
