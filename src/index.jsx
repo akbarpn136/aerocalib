2ecbfd8dcf532d0f8765c42934a9662c43062edf
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-08-24 22:27:14 +0700

    Penyesuaian muat style

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 /* @refresh reload */
 import { render } from "solid-js/web"
 
-import "./index.css"
+import "./styles/index.css"
 import MainRoutes from "./routes"
 
 render(() => <MainRoutes />, document.getElementById("root"))
