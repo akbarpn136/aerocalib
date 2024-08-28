92aeb41e7e04d992e63178be41c66f60a592909c
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-08-28 18:29:08 +0700

    Daftar state global dengan context

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,22 +1,26 @@
 import { lazy } from "solid-js"
 import { Router, Route } from "@solidjs/router"
 
+import { AppContext } from "../stores"
 import Default from "../layouts/default"
-import HalamanUtama from "../pages/utama"
 import HalamanKosong from "../pages/404"
+import HalamanUtama from "../pages/utama"
+import {state, setState} from "../stores"
 
 export default function MainRoutes() {
     const Arsip = lazy(() => import("../pages/arsip"))
     const Tentang = lazy(() => import("../pages/tentang"))
     const Rincian = lazy(() => import("../pages/rincian"))
 
-    return <Router>
-        <Route path="/" component={Default}>
-            <Route path="/" component={HalamanUtama} />
-            <Route path="/:id/rincian" component={<Rincian />} />
-            <Route path="/arsip" component={<Arsip />} />
-            <Route path="/tentang" component={<Tentang />} />
-        </Route>
-        <Route path="*404" component={HalamanKosong} />
-    </Router>
+    return <AppContext.Provider value={{state, setState}}>
+        <Router>
+            <Route path="/" component={Default}>
+                <Route path="/" component={HalamanUtama} />
+                <Route path="/:id/rincian" component={<Rincian />} />
+                <Route path="/arsip" component={<Arsip />} />
+                <Route path="/tentang" component={<Tentang />} />
+            </Route>
+            <Route path="*404" component={HalamanKosong} />
+        </Router>
+    </AppContext.Provider>
 }
