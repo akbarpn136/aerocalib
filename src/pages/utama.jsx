cfb5040ab65f9a948a7b209076aff9cef02aa425
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-08-29 19:33:01 +0700

    Pengkondisian ketika data kosong

--- src/pages/utama.jsx
+++ src/pages/utama.jsx
@@ -1,13 +1,14 @@
 import { A } from "@solidjs/router"
-import { For, Show, createEffect, createSignal, useContext } from "solid-js"
+import { For, Show, Switch, Match, createEffect, createSignal, useContext } from "solid-js"
 
 import { AppContext } from "../stores"
 import { produce } from "solid-js/store"
 
 export default function HalamanUtama() {
     const [page, setPage] = createSignal(1)
-    const [selanjutnya, setSelanjutnya] = createSignal(true)
+    const [kosong, setKosong] = createSignal(true)
     const [sebelumnya, setSebelumnya] = createSignal(true)
+    const [selanjutnya, setSelanjutnya] = createSignal(true)
 
     const { state, setState } = useContext(AppContext)
 
@@ -45,6 +46,12 @@ export default function HalamanUtama() {
                 throw err
             }
 
+            if (state.kegiatan.length == 0) {
+                setKosong(true)
+            } else {
+                setKosong(false)
+            }
+
             if (state.kegiatan.length < limit) {
                 setSelanjutnya(false)
             } else {
@@ -73,44 +80,69 @@ export default function HalamanUtama() {
                         Dibuat
                     </th>
                     <th scope="col" class="px-6 py-3">
-                        Action
+                        Opsi
                     </th>
                 </tr>
             </thead>
             <tbody>
-                <For each={state.kegiatan}>{(item, index) => {
-                    return <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            {item.peralatan}
-                        </th>
-                        <td class="px-6 py-4">
-                            {item.instansi}
-                        </td>
-                        <td class="px-6 py-4">
-                            {
-                                item.dibuat.toLocaleString("id-id", {
-                                    weekday: "long",
-                                    year: "numeric",
-                                    month: "long",
-                                    day: "numeric",
-                                    hour: "2-digit",
-                                    minute: "2-digit",
-                                })
-                            }
-                        </td>
-                        <td class="px-6 py-4">
-                            <A href={`${item.id.id}/rincian`} class="flex items-center justify-center w-9 h-9 text-xs font-medium text-gray-700 bg-white border border-gray-200 rounded-lg toggle-tablet-view hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-gray-300 dark:focus:ring-gray-500 dark:bg-gray-800 focus:outline-none dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                                <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M12 6h.01M12 12h.01M12 18h.01" />
-                                </svg>
-                            </A>
-                        </td>
-                    </tr>
-                }}</For>
+                <Switch>
+                    <Match when={!kosong()}>
+                        <For each={state.kegiatan}>{(item, index) => {
+                            return <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {item.peralatan}
+                                </th>
+                                <td class="px-6 py-4">
+                                    {item.instansi}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {
+                                        item.dibuat.toLocaleString("id-id", {
+                                            weekday: "long",
+                                            year: "numeric",
+                                            month: "long",
+                                            day: "numeric",
+                                            hour: "2-digit",
+                                            minute: "2-digit",
+                                        })
+                                    }
+                                </td>
+                                <td class="px-6 py-4">
+                                    <A href={`${item.id.id}/rincian`} class="flex items-center justify-center w-9 h-9 text-xs font-medium text-gray-700 bg-white border border-gray-200 rounded-lg toggle-tablet-view hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-gray-300 dark:focus:ring-gray-500 dark:bg-gray-800 focus:outline-none dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                                        <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M12 6h.01M12 12h.01M12 18h.01" />
+                                        </svg>
+                                    </A>
+                                </td>
+                            </tr>
+                        }}</For>
+                    </Match>
+
+                    <Match when={kosong()}>
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                -
+                            </th>
+                            <td class="px-6 py-4">
+                                -
+                            </td>
+                            <td class="px-6 py-4">
+                                -
+                            </td>
+                            <td class="px-6 py-4">
+                                -
+                            </td>
+                        </tr>
+                    </Match>
+                </Switch>
             </tbody>
         </table>
 
         <nav class="flex items-center flex-column flex-wrap md:flex-row justify-between py-4" aria-label="Table navigation">
+            <Show when={kosong()}>
+                <span class="ms-5 text-sm text-gray-500 dark:text-gray-300">Kegiatan masih kosong.</span>
+            </Show>
+
             <ul class="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8 ms-5">
                 <Show when={sebelumnya()}>
                     <li>
