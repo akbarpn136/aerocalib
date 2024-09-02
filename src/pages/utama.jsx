02cd5f53e37053954ca6c7cb92291fde18524eb7
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-09-02 13:32:38 +0700

    Pekerjaan lanjutan untuk pencarian

--- src/pages/utama.jsx
+++ src/pages/utama.jsx
@@ -3,10 +3,10 @@ import { For, Show, Switch, Match, createEffect, createSignal, useContext } from
 
 import { AppContext } from "../stores"
 import { produce } from "solid-js/store"
+import { cariKegiatan, filterKegiatan } from "../handlers/kegiatan"
 
 export default function HalamanUtama() {
     const [page, setPage] = createSignal(1)
-    const [cari, setCari] = createSignal(null)
     const [kosong, setKosong] = createSignal(true)
     const [sebelumnya, setSebelumnya] = createSignal(true)
     const [selanjutnya, setSelanjutnya] = createSignal(true)
@@ -25,26 +25,64 @@ export default function HalamanUtama() {
         }
     }
 
+    const onFilterKegiatan = async (db, page, limit) => {
+        try {
+            const hasil = await filterKegiatan(
+                db,
+                page,
+                limit
+            )
+
+            setState("kegiatan", [])  // reset empty dulu array
+            setState("kegiatan", produce((keg) => {
+                hasil.forEach(el => {
+                    keg.push(el)
+                })
+            }))
+        } catch (err) {
+            throw err
+        }
+    }
+
     const onCariPeralatan = async (e) => {
-        const db = state.surreal
         const cari = e.currentTarget.value
         const limit = Number(import.meta.env.VITE_LIMIT_KEGIATAN)
 
         if (e.keyCode == 13) {
-            try {
-                const result = await db.query(
-                    "SELECT id, peralatan, instansi, dibuat, search::score(1) AS score_peralatan, search::score(2) AS score_instansi FROM kegiatan WHERE peralatan @1@ $cari OR instansi @2@ $cari ORDER BY dibuat DESC LIMIT $limit START ($page - 1) * $limit;",
-                    { page: page(), limit, cari }
-                )
+            if (cari == "") {
+                setPage(1)
+                try {
+                    await onFilterKegiatan(
+                        state.surreal,
+                        page(),
+                        limit
+                    )
+                } catch (err) {
+                    throw err
+                }
+            } else {
+                try {
+                    const pencarian = await cariKegiatan(
+                        state.surreal,
+                        page(),
+                        limit,
+                        cari
+                    )
     
-                setState("kegiatan", [])  // reset empty dulu array
-                setState("kegiatan", produce((keg) => {
-                    result[0].forEach(el => {
-                        keg.push(el)
-                    })
-                }))
-            } catch (err) {
-                throw err
+                    if (pencarian.length > 0) {
+                        setKosong(false)
+                        setState("kegiatan", [])  // reset empty dulu array
+                        setState("kegiatan", produce((keg) => {
+                            pencarian.forEach(el => {
+                                keg.push(el)
+                            })
+                        }))
+                    } else {
+                        setKosong(true)
+                    }
+                } catch (err) {
+                    throw err
+                }
             }
         }
     }
@@ -56,17 +94,7 @@ export default function HalamanUtama() {
             const limit = Number(import.meta.env.VITE_LIMIT_KEGIATAN)
 
             try {
-                const result = await db.query(
-                    "SELECT id, peralatan, instansi, dibuat FROM kegiatan WHERE arsip = false ORDER BY dibuat DESC LIMIT $limit START ($page - 1) * $limit;",
-                    { page: page(), limit }
-                )
-
-                setState("kegiatan", [])  // reset empty dulu array
-                setState("kegiatan", produce((keg) => {
-                    result[0].forEach(el => {
-                        keg.push(el)
-                    })
-                }))
+                await onFilterKegiatan(db, page(), limit)
             } catch (err) {
                 throw err
             }
@@ -165,11 +193,11 @@ export default function HalamanUtama() {
 
         <nav class="flex items-center flex-column flex-wrap md:flex-row justify-between py-4" aria-label="Table navigation">
             <Show when={kosong()}>
-                <span class="ms-5 text-sm text-gray-500 dark:text-gray-300">Kegiatan masih kosong.</span>
+                <span class="ms-5 text-sm text-gray-500 dark:text-gray-300">Kegiatan tidak ada.</span>
             </Show>
 
             <Show when={state.kegiatan.length > 0}>
-                <div class="ms-5 bg-white dark:bg-gray-900">
+                <div class="mx-5 bg-white dark:bg-gray-900">
                     <label for="table-search" class="sr-only">Temukan kegiatan...</label>
                     <div class="relative mt-1">
                         <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -187,43 +215,45 @@ export default function HalamanUtama() {
                 </div>
             </Show>
 
-            <ul class="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8 me-5">
-                <Show when={sebelumnya()}>
-                    <li>
-                        <button
-                            type="button"
-                            class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                            classList={{
-                                "rounded-s-lg": state.kegiatan.length == Number(import.meta.env.VITE_LIMIT_KEGIATAN),
-                                "rounded-lg": state.kegiatan.length < Number(import.meta.env.VITE_LIMIT_KEGIATAN)
-                            }}
-                            onClick={prevPage}
-                        >
-                            <svg class="w-5 h-5 text-gray-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m15 19-7-7 7-7" />
-                            </svg>
-                        </button>
-                    </li>
-                </Show>
+            <Show when={!kosong()}>
+                <ul class="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8 me-5">
+                    <Show when={sebelumnya()}>
+                        <li>
+                            <button
+                                type="button"
+                                class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                                classList={{
+                                    "rounded-s-lg": state.kegiatan.length == Number(import.meta.env.VITE_LIMIT_KEGIATAN),
+                                    "rounded-lg": state.kegiatan.length < Number(import.meta.env.VITE_LIMIT_KEGIATAN)
+                                }}
+                                onClick={prevPage}
+                            >
+                                <svg class="w-5 h-5 text-gray-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m15 19-7-7 7-7" />
+                                </svg>
+                            </button>
+                        </li>
+                    </Show>
 
-                <Show when={selanjutnya()}>
-                    <li>
-                        <button
-                            type="button"
-                            class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                            classList={{
-                                "rounded-e-lg": page() > 1,
-                                "rounded-lg": page() == 1
-                            }}
-                            onClick={nextPage}
-                        >
-                            <svg class="w-5 h-5 text-gray-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m9 5 7 7-7 7" />
-                            </svg>
-                        </button>
-                    </li>
-                </Show>
-            </ul>
+                    <Show when={selanjutnya()}>
+                        <li>
+                            <button
+                                type="button"
+                                class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                                classList={{
+                                    "rounded-e-lg": page() > 1,
+                                    "rounded-lg": page() == 1
+                                }}
+                                onClick={nextPage}
+                            >
+                                <svg class="w-5 h-5 text-gray-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m9 5 7 7-7 7" />
+                                </svg>
+                            </button>
+                        </li>
+                    </Show>
+                </ul>
+            </Show>
         </nav>
     </div>
 }
