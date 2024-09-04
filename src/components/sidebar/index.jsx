44ed6a7b13d6fabee0af1870a79fb7c8a627c377
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-09-04 10:40:13 +0700

    Hapus tautan yang tidak digunakan

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -17,16 +17,6 @@ export default function Sidebar() {
                     </li>
 
                     <li>
-                        <A href="/arsip" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group" activeClass="bg-gray-100 dark:bg-gray-700">
-                            <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linejoin="round" stroke-width="2" d="M10 12v1h4v-1m4 7H6a1 1 0 0 1-1-1V9h14v9a1 1 0 0 1-1 1ZM4 5h16a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Z" />
-                            </svg>
-
-                            <span class="ms-3">Arsip</span>
-                        </A>
-                    </li>
-
-                    <li>
                         <A href="/tentang" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group" activeClass="bg-gray-100 dark:bg-gray-700">
                             <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
