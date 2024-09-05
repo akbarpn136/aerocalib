6b8920495de8f23c83a38b51323fda24eda61e64
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-09-05 19:44:41 +0700

    Penyederhanaan struktur layout utama

--- src/layouts/default.jsx
+++ src/layouts/default.jsx
@@ -1,42 +1,42 @@
-import "flowbite"
-import { onMount, ErrorBoundary, createSignal, Show, useContext } from "solid-js"
+import "flowbite";
+import { Show, onMount, useContext, createSignal } from "solid-js";
 
-import { initDb } from "../configs/db"
-import { AppContext } from "../stores"
-import Navbar from "../components/navbar"
-import Sidebar from "../components/sidebar"
-import ToastSalah from "../components/toast/salah"
+import { initDb } from "../configs/db";
+import { AppContext } from "../stores";
+import Navbar from "../components/navbar";
+import Sidebar from "../components/sidebar";
+import ToastSalah from "../components/toast/salah";
 
 export default function Default(props) {
-    const [pesan, setPesan] = createSignal("")
-    const [salah, setSalah] = createSignal(false)
+  const [pesan, setPesan] = createSignal("");
+  const [salah, setSalah] = createSignal(false);
 
-    const {_, setState} = useContext(AppContext)
+  const { _, setState } = useContext(AppContext);
 
-    onMount(async () => {
-        try {
-            const conn = await initDb()
+  onMount(async () => {
+    try {
+      const conn = await initDb();
 
-            setState("surreal", conn)
-        } catch (err) {
-            setPesan(err.message)
-            setSalah(true)
-        }
-    })
+      setState("surreal", conn);
+    } catch (err) {
+      setPesan(err.message);
+      setSalah(true);
+    }
+  });
 
-    return <div>
-        <Navbar />
+  return (
+    <div>
+      <Navbar />
 
-        <Sidebar />
+      <Sidebar />
 
-        <div class="p-4 sm:ml-64">
-            <Show when={salah()}>
-                <ToastSalah pesan={pesan()} />
-            </Show>
+      <div class="p-4 sm:ml-64">
+        <Show when={salah()}>
+          <ToastSalah pesan={pesan()} />
+        </Show>
 
-            <ErrorBoundary fallback={(err) => <ToastSalah pesan={err.message} />}>
-                {props.children}
-            </ErrorBoundary>
-        </div>
+        {props.children}
+      </div>
     </div>
+  );
 }
