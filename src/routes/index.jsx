3717fb0f72a55cd2f4c6743c552ebc1d4c81b560
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-09-06 18:04:38 +0700

    Modifikasi lanjut router

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,24 +1,26 @@
-import { lazy } from "solid-js"
-import { Router, Route } from "@solidjs/router"
+import { lazy } from "solid-js";
+import { Router, Route } from "@solidjs/router";
 
-import { AppContext } from "../stores"
-import Default from "../layouts/default"
-import HalamanKosong from "../pages/404"
-import HalamanUtama from "../pages/utama"
-import {state, setState} from "../stores"
+import { AppContext } from "../stores";
+import Default from "../layouts/default";
+import HalamanKosong from "../pages/404";
+import HalamanUtama from "../pages/utama";
+import { state, setState } from "../stores";
+import HalamanRincian from "../pages/rincian";
 
 export default function MainRoutes() {
-    const Tentang = lazy(() => import("../pages/tentang"))
-    const Rincian = lazy(() => import("../pages/rincian"))
+  const Tentang = lazy(() => import("../pages/tentang"));
 
-    return <AppContext.Provider value={{state, setState}}>
-        <Router>
-            <Route path="/" component={Default}>
-                <Route path="/" component={HalamanUtama} />
-                <Route path="/:id/rincian" component={<Rincian />} />
-                <Route path="/tentang" component={<Tentang />} />
-            </Route>
-            <Route path="*404" component={HalamanKosong} />
-        </Router>
+  return (
+    <AppContext.Provider value={{ state, setState }}>
+      <Router>
+        <Route path="/" component={Default}>
+          <Route path="/" component={HalamanUtama} />
+          <Route path="/:id/rincian" component={HalamanRincian} />
+          <Route path="/tentang" component={<Tentang />} />
+        </Route>
+        <Route path="*404" component={HalamanKosong} />
+      </Router>
     </AppContext.Provider>
+  );
 }
