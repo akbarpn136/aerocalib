d4af3f7f05e8558ee55bf40049ff35bda7c53233
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-09-11 13:23:46 +0700

    Pengkondisian rendering

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,11 @@
 /* @refresh reload */
-import { render } from "solid-js/web"
+import { render } from "solid-js/web";
 
-import "./styles/index.css"
-import MainRoutes from "./routes"
+import "./styles/index.css";
+import MainRoutes from "./routes";
 
-render(() => <MainRoutes />, document.getElementById("root"))
+const dispose = render(() => <MainRoutes />, document.getElementById("root"));
+
+if (import.meta.hot) {
+  import.meta.hot.dispose(dispose);
+}
