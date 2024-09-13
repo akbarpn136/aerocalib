6eafc302cc83503fa80b3e60c4d3e55a244d7427
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-09-13 09:12:22 +0700

    Koreksi render button navigasi

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -33,94 +33,36 @@ export default function Navbar() {
 
   return (
     <div class="mt-2">
-      <button
-        data-drawer-target="default-sidebar"
-        data-drawer-toggle="default-sidebar"
-        aria-controls="default-sidebar"
-        type="button"
-        class="inline-flex items-center ms-4 px-4 py-2 text-sm shadow-sm border border-gray-200 text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-      >
-        <span class="sr-only">Open sidebar</span>
-
-        <Menu size={19} />
-      </button>
-
-      <div class="inline-flex mx-2 sm:ml-16 sm:px-4">
-        <div class="inline-flex items-center rounded-md shadow-sm" role="group">
-          <Switch>
-            <Match when={path() == "/"}>
-              <button
-                data-tooltip-target="tooltip-tambah-kegiatan"
-                type="button"
-                class="inline-flex items-center px-4 py-2 text-sm text-gray-500 border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-gray-900 focus:z-10 focus:ring-2 focus:ring-gray-700 focus:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-500 dark:focus:text-white"
-                onClick={onShowModal}
-              >
+      <div class="inline-flex ml-16 px-4 gap-x-2">
+        <Switch>
+          <Match when={path() == "/"}>
+            <div class="tooltip tooltip-right" data-tip="Tambah kegiatan">
+              <button type="button" class="btn btn-sm" onClick={onShowModal}>
                 <Plus size={19} />
               </button>
-              <div
-                id="tooltip-tambah-kegiatan"
-                role="tooltip"
-                class="absolute z-50 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
-              >
-                Tambah kegiatan
-                <div class="tooltip-arrow" data-popper-arrow></div>
-              </div>
+            </div>
 
-              <button
-                data-tooltip-target="tooltip-muat-kegiatan"
-                type="button"
-                class="inline-flex items-center px-4 py-2 text-sm text-gray-500 border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-gray-900 focus:z-10 focus:ring-2 focus:ring-gray-700 focus:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-500 dark:focus:text-white"
-                onClick={onClickArsip}
-              >
+            <div class="tooltip tooltip-right" data-tip="Arsip kegiatan">
+              <button type="button" class="btn btn-sm" onClick={onClickArsip}>
                 <Archive size={19} />
               </button>
-              <div
-                id="tooltip-muat-kegiatan"
-                role="tooltip"
-                class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
-              >
-                Arsip kegiatan
-                <div class="tooltip-arrow" data-popper-arrow></div>
-              </div>
-            </Match>
+            </div>
+          </Match>
 
-            <Match when={path().includes("/rincian")}>
-              <button
-                data-tooltip-target="tooltip-ubah-kegiatan"
-                type="button"
-                class="inline-flex items-center px-4 py-2 text-sm text-gray-500 border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-gray-900 focus:z-10 focus:ring-2 focus:ring-gray-700 focus:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-500 dark:focus:text-white"
-                onClick={onShowModal}
-              >
+          <Match when={path().includes("/rincian")}>
+            <div class="tooltip tooltip-right" data-tip="Ubah kegiatan">
+              <button type="button" class="btn btn-sm" onClick={onShowModal}>
                 <Pencil size={19} />
               </button>
-              <div
-                id="tooltip-ubah-kegiatan"
-                role="tooltip"
-                class="absolute z-50 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
-              >
-                Ubah kegiatan
-                <div class="tooltip-arrow" data-popper-arrow></div>
-              </div>
+            </div>
 
-              <button
-                data-tooltip-target="tooltip-arsipkan-kegiatan"
-                type="button"
-                class="inline-flex items-center px-4 py-2 text-sm text-gray-500 border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-gray-900 focus:z-10 focus:ring-2 focus:ring-gray-700 focus:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-500 dark:focus:text-white"
-                onClick={onClickArsip}
-              >
+            <div class="tooltip tooltip-right" data-tip="Arsipkan kegiatan">
+              <button type="button" class="btn btn-sm" onClick={onShowModal}>
                 <ArchiveX size={19} />
               </button>
-              <div
-                id="tooltip-arsipkan-kegiatan"
-                role="tooltip"
-                class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
-              >
-                Arsipkan kegiatan
-                <div class="tooltip-arrow" data-popper-arrow></div>
-              </div>
-            </Match>
-          </Switch>
-        </div>
+            </div>
+          </Match>
+        </Switch>
       </div>
 
       <Show when={showModal()}>
