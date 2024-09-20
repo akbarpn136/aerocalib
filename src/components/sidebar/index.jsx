cf7069fd2be36570f7763ecc1a3be328cc128e89
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-09-20 22:10:06 +0700

    Modifikasi render sidebar nav

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -6,25 +6,14 @@ export default function Sidebar() {
   return (
     <aside class="z-10 fixed top-0 left-0 h-screen" aria-label="Sidebar">
       <ul class="menu bg-base-300 h-full rounded-none space-y-1 font-medium">
-        <li>
-          <A
-            href="/"
-            end
-            class="tooltip tooltip-right"
-            data-tip="Kegiatan"
-            activeClass="bg-primary text-base-100"
-          >
+        <li class="tooltip tooltip-right" data-tip="Kegiatan">
+          <A href="/" end class="btn" activeClass="btn-primary">
             <MonitorCog size={19} />
           </A>
         </li>
 
-        <li>
-          <A
-            href="/tentang"
-            class="tooltip tooltip-right"
-            data-tip="Tentang"
-            activeClass="bg-primary text-base-100"
-          >
+        <li class="tooltip tooltip-right" data-tip="Tentang">
+          <A href="/tentang" class="btn" activeClass="btn-primary">
             <Info size={19} />
           </A>
         </li>
