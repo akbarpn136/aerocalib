4f776f7dbb7e33d89888b25fc871bef923561e10
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-09-21 17:32:26 +0700

    Penyederhanaan handler kegiatan

--- src/lib/handlers/kegiatan.js
+++ src/lib/handlers/kegiatan.js
@@ -1,48 +1,32 @@
-export const buatKegiatan = async (db, {peralatan, instansi}) => {
-    try {
-        const result = await db.query(
-            "CREATE kegiatan SET peralatan = $peralatan, instansi = $instansi, dibuat = time::now(), arsip = false;",
-            { peralatan, instansi }
-        )
+export const buatKegiatan = async ({db, peralatan, instansi}) => {
+  const result = await db.query(
+    "CREATE kegiatan SET peralatan = $peralatan, instansi = $instansi, dibuat = time::now(), arsip = false;",
+    { peralatan, instansi }
+  )
 
-        return result[0]
-    } catch (err) {
-        throw err
-    }
+  return result[0]
 }
 
 export const filterKegiatan = async (db, page, limit, arsip=false) => {
-    try {
-        const result = await db.query(
-            "SELECT id, peralatan, instansi, dibuat FROM kegiatan WHERE arsip = $arsip ORDER BY dibuat DESC LIMIT $limit START ($page - 1) * $limit;",
-            { page, limit, arsip }
-        )
+  const result = await db.query(
+    "SELECT id, peralatan, instansi, dibuat FROM kegiatan WHERE arsip = $arsip ORDER BY dibuat DESC LIMIT $limit START ($page - 1) * $limit;",
+    { page, limit, arsip }
+  )
 
-        return result[0]
-    } catch (err) {
-        throw err
-    }
+  return result[0]
 }
 
 export const cariKegiatan = async (db, page, limit, cari, arsip=false) => {
-    try {
-        const result = await db.query(
-            "SELECT id, peralatan, instansi, dibuat, search::score(1) AS score_peralatan, search::score(2) AS score_instansi FROM kegiatan WHERE arsip = $arsip AND peralatan @1@ $cari OR instansi @2@ $cari ORDER BY dibuat DESC LIMIT $limit START ($page - 1) * $limit;",
-            { page, limit, cari, arsip }
-        )
+  const result = await db.query(
+    "SELECT id, peralatan, instansi, dibuat, search::score(1) AS score_peralatan, search::score(2) AS score_instansi FROM kegiatan WHERE arsip = $arsip AND peralatan @1@ $cari OR instansi @2@ $cari ORDER BY dibuat DESC LIMIT $limit START ($page - 1) * $limit;",
+    { page, limit, cari, arsip }
+  )
 
-        return result[0]
-    } catch (err) {
-        throw err
-    }
+  return result[0]
 }
 
 export const filterKegiatanId = async (db, id) => {
-    try {
-        const result = await db.query("SELECT * FROM kegiatan WHERE id = $id;", { id })
+  const result = await db.query("SELECT * FROM kegiatan WHERE id = $id;", { id })
 
-        return result[0]
-    } catch (err) {
-        throw err
-    }
+  return result[0]
 }
