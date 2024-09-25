32efe4132d0f3c0af6f52126a460c8a2b0235a36
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-09-25 09:44:28 +0700

    Modifikasi fungsi cari

--- src/lib/handlers/kegiatan.js
+++ src/lib/handlers/kegiatan.js
@@ -16,10 +16,10 @@ export const filterKegiatan = async (db, page, limit, arsip=false) => {
   return result[0]
 }
 
-export const cariKegiatan = async (db, page, limit, cari, arsip=false) => {
+export const cariKegiatan = async (db, page, limit, cari) => {
   const result = await db.query(
-    "SELECT id, peralatan, instansi, dibuat, search::score(1) AS score_peralatan, search::score(2) AS score_instansi FROM kegiatan WHERE arsip = $arsip AND peralatan @1@ $cari OR instansi @2@ $cari ORDER BY dibuat DESC LIMIT $limit START ($page - 1) * $limit;",
-    { page, limit, cari, arsip }
+    "SELECT id, peralatan, instansi, dibuat, search::score(1) AS score_peralatan, search::score(2) AS score_instansi FROM kegiatan WHERE peralatan @1@ $cari OR instansi @2@ $cari ORDER BY dibuat DESC LIMIT $limit START ($page - 1) * $limit;",
+    { page, limit, cari }
   )
 
   return result[0]
