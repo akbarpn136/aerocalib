f779e5f1f19bcf6a33f060110536ed8209936315
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-09-26 22:23:51 +0700

    Menerapkan komponen kalibrasi

--- src/pages/rincian.jsx
+++ src/pages/rincian.jsx
@@ -5,12 +5,30 @@ export default function HalamanRincian() {
   const DefaultStatsComponent = lazy(() =>
     import("../components/rincian/stats")
   );
+  const DefaultTableComponent = lazy(() =>
+    import("../components/rincian/tabel")
+  );
+  const OlahSensorComponent = lazy(() => import("../components/rincian/olah"));
 
   return (
     <div class="space-y-8 h-[calc(100vh-505px)] md:h-[calc(100vh-230px)]">
       <DefaultStatsComponent />
 
       <DefaultPlotComponent />
+
+      <DefaultTableComponent />
+
+      <dialog id="modal_olah_sensor" class="modal">
+        <div class="modal-box w-11/12 max-w-5xl space-y-6">
+          <h3 class="text-lg font-bold">Olah Data Kalibrasi</h3>
+
+          <OlahSensorComponent />
+        </div>
+
+        <form method="dialog" class="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
     </div>
   );
 }
