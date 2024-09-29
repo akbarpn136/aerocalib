4f306ae77df08eb614f8664b87bc2b96f26959c7
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-09-29 19:12:54 +0700

    Mendaftarkan sidecar surreal

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,27 @@
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri_plugin_shell::ShellExt;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let sidecar_command = app
+                .shell()
+                .sidecar("surreal")
+                .expect("failed to create `surreal` binary command")
+                .args([
+                    "start",
+                    "--user",
+                    "root",
+                    "--pass",
+                    "root",
+                    "file:aerocalib.db",
+                ]);
+
+            sidecar_command.spawn().expect("Failed to spawn surreal");
+
+            Ok(())
+        })
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
