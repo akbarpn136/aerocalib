1f1e4b1338f208a50e7939e06f9053619cd90b21
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-10-13 18:53:40 +0700

    Modifikasi lanjut rancangan plot kalibrasi

--- src/components/rincian/plot.jsx
+++ src/components/rincian/plot.jsx
@@ -1,61 +1,184 @@
-import { onMount } from "solid-js";
+import { onMount, createEffect } from "solid-js";
 import Plotly from "plotly.js-dist-min";
 import { useSearchParams } from "@solidjs/router";
 
 export default function DefaultPlot() {
   let plot1;
 
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const configs = {
-    responsive: true,
-    scrollZoom: true,
-    displayModeBar: false,
-    displaylogo: false,
-  };
-
-  const yaxis_name =
-    searchParams.kalibrasi === "tekanan" ? "Tekanan (Pa)" : "Kecepatan (m/s)";
-  const layout1 = {
-    title: "Distribusi Data Pengujian Kalibrasi",
-    responsive: true,
-    showlegend: true,
-    legend: { orientation: "v" },
-    paper_bgcolor: "#eceff4",
-    plot_bgcolor: "#eceff4",
-    margin: { l: 65, r: 65, b: 75, t: 55, pad: 10 },
-    xaxis: { title: "Frekuensi (Hz)" },
-    yaxis: {
-      title: yaxis_name,
-    },
-    yaxis2: {
-      title: yaxis_name,
-      titlefont: { color: "rgb(148, 103, 189)" },
-      tickfont: { color: "rgb(148, 103, 189)" },
-      overlaying: "y",
-      side: "right",
-    },
-  };
-
-  const trace1 = {
-    x: [1, 2, 3],
-    y: [40, 50, 60],
-    name: "LA3",
-    type: "scatter",
-    mode: "markers",
-  };
-
-  const trace2 = {
-    x: [2, 3, 4],
-    y: [4, 5, 6],
-    name: "Client",
-    yaxis: "y2",
-    type: "scatter",
-    mode: "markers",
-  };
+  const [searchParams] = useSearchParams();
 
   onMount(() => {
-    Plotly.newPlot(plot1, [trace1, trace2], layout1, configs);
+    let layout;
+    let traces = [];
+
+    const jenis_kalibrasi = searchParams.kalibrasi;
+    const initlayout = {
+      title: "Distribusi Data Pengujian Kalibrasi",
+      responsive: true,
+      showlegend: false,
+      legend: { orientation: "v" },
+      paper_bgcolor: "#eceff4",
+      plot_bgcolor: "#eceff4",
+      margin: { l: 65, r: 65, b: 75, t: 50, pad: 10 },
+    };
+
+    const configs = {
+      responsive: true,
+      scrollZoom: false,
+      displayModeBar: false,
+      displaylogo: false,
+    };
+
+    switch (jenis_kalibrasi) {
+      case "tekanan":
+        layout = {
+          ...initlayout,
+          xaxis: { title: "Frekuensi (Hz)" },
+          yaxis: {
+            title: "Tekanan (Pa)",
+          },
+
+          xaxis2: { title: "Frekuensi (Hz)" },
+          yaxis2: {
+            title: "Tekanan (Pa)",
+          },
+          grid: { rows: 1, columns: 2, pattern: "independent" },
+        };
+
+        const trace1 = {
+          x: [1, 2, 3],
+          y: [40, 50, 60],
+          name: "LA3",
+          type: "scatter",
+          mode: "markers",
+          xaxis: "x",
+          yaxis: "y",
+        };
+
+        const trace2 = {
+          x: [2, 3, 4],
+          y: [4, 5, 6],
+          name: "Client",
+          type: "scatter",
+          mode: "markers",
+          xaxis: "x2",
+          yaxis: "y2",
+        };
+
+        traces.push(trace1, trace2);
+
+        break;
+
+      case "kecepatan":
+        layout = {
+          ...initlayout,
+          xaxis: { title: "Frekuensi (Hz)" },
+          yaxis: {
+            title: "Kecepatan (m/s)",
+          },
+
+          xaxis2: { title: "Frekuensi (Hz)" },
+          yaxis2: {
+            title: "Kecepatan (m/s)",
+          },
+          grid: { rows: 1, columns: 2, pattern: "independent" },
+        };
+
+        const trace3 = {
+          x: [1, 2, 3],
+          y: [0, 15, 45],
+          name: "LA32",
+          type: "scatter",
+          mode: "markers",
+          xaxis: "x",
+          yaxis: "y",
+        };
+
+        const trace4 = {
+          x: [2, 3, 4],
+          y: [0, 8, 6],
+          name: "Client2",
+          type: "scatter",
+          mode: "markers",
+          xaxis: "x2",
+          yaxis: "y2",
+        };
+
+        traces.push(trace3, trace4);
+
+        break;
+
+      default:
+        layout = {
+          ...initlayout,
+          xaxis: { title: "Frekuensi (Hz)" },
+          yaxis: {
+            title: "Tekanan (Pa)",
+          },
+
+          xaxis2: { title: "Frekuensi (Hz)" },
+          yaxis2: {
+            title: "Tekanan (Pa)",
+          },
+
+          xaxis3: { title: "Frekuensi (Hz)" },
+          yaxis3: {
+            title: "Kecepatan (m/s)",
+          },
+          xaxis4: { title: "Frekuensi (Hz)" },
+          yaxis4: {
+            title: "Kecepatan (m/s)",
+          },
+
+          grid: { rows: 2, columns: 2, pattern: "independent" },
+        };
+
+        const trace5 = {
+          x: [1, 2, 3],
+          y: [40, 50, 60],
+          name: "LA3",
+          type: "scatter",
+          mode: "markers",
+          xaxis: "x",
+          yaxis: "y",
+        };
+
+        const trace6 = {
+          x: [2, 3, 4],
+          y: [4, 5, 6],
+          name: "Client",
+          type: "scatter",
+          mode: "markers",
+          xaxis: "x2",
+          yaxis: "y2",
+        };
+
+        const trace7 = {
+          x: [1, 2, 3],
+          y: [0, 15, 45],
+          name: "LA32",
+          type: "scatter",
+          mode: "markers",
+          xaxis: "x3",
+          yaxis: "y3",
+        };
+
+        const trace8 = {
+          x: [2, 3, 4],
+          y: [0, 8, 6],
+          name: "Client2",
+          type: "scatter",
+          mode: "markers",
+          xaxis: "x4",
+          yaxis: "y4",
+        };
+
+        traces.push(trace5, trace6, trace7, trace8);
+
+        break;
+    }
+
+    Plotly.newPlot(plot1, traces, layout, configs);
   });
 
   return <div ref={plot1} class="w-full h-full border shadow" />;
