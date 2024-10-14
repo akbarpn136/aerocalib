1095ded2eb567bed6994b7dda4570a1470834c05
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-10-14 13:30:30 +0700

    Modifikasi lanjutan untuk halaman utama

--- src/pages/utama.jsx
+++ src/pages/utama.jsx
@@ -1,7 +1,8 @@
-import { produce } from "solid-js/store";
+import { createStore, produce } from "solid-js/store";
 import { A, useSearchParams } from "@solidjs/router";
 import {
   Search,
+  CircleX,
   ChevronLeft,
   ChevronRight,
   EllipsisVertical,
@@ -11,18 +12,31 @@ import {
   Show,
   Switch,
   Match,
+  Suspense,
   useContext,
   createEffect,
   createSignal,
+  createResource,
+  lazy,
 } from "solid-js";
 
 import { AppContext } from "../stores";
 import { cariKegiatan, filterKegiatan } from "../lib/handlers/kegiatan";
 
 export default function HalamanUtama() {
+  const SkeletonUtama = lazy(() => import("../components/kegiatan/skeleton"));
+
   const limit = Number(import.meta.env.VITE_LIMIT_KEGIATAN);
 
+  const { state, setState } = useContext(AppContext);
   const [searchParams, setSearchParams] = useSearchParams();
+  const [store, setStore] = createStore({
+    page: 1,
+    cari: "",
+    arsip: false,
+    sebelumnya: true,
+    selanjutnya: true,
+  });
 
   const [page, setPage] = createSignal(1);
   const [kata, setKata] = createSignal("");
@@ -30,33 +44,44 @@ export default function HalamanUtama() {
   const [sebelumnya, setSebelumnya] = createSignal(true);
   const [selanjutnya, setSelanjutnya] = createSignal(true);
 
-  const { state, setState } = useContext(AppContext);
+  const bacaKegiatan = async ({ db, page, limit, cari, arsip }) => {
+    let hasil;
+
+    if (cari !== "") {
+      hasil = await cariKegiatan(db, page, limit, cari);
+    } else {
+      hasil = await filterKegiatan(db, page, limit, arsip);
+    }
+
+    return hasil;
+  };
+
+  const [kegiatan, { refetch }] = createResource(
+    {
+      limit,
+      db: state.surreal,
+      page: store.page,
+      cari: store.cari,
+      arsip: store.arsip,
+    },
+    bacaKegiatan
+  );
 
   const nextPage = () => {
-    setPage(page() + 1);
+    setStore("page", store.page + 1);
 
-    if (searchParams.arsip) {
-      setSearchParams({
-        pagekegiatan: page(),
-      });
-    } else {
-      setSearchParams({
-        pagekegiatan: page(),
-        arsip: false,
-      });
-    }
+    refetch();
   };
 
   const prevPage = () => {
     if (page() > 1) {
       setPage(page() - 1);
+      setStore("page", store.page - 1);
     } else {
-      setPage(1);
+      setStore("page", 1);
     }
 
-    setSearchParams({
-      pagekegiatan: page(),
-    });
+    refetch();
   };
 
   const onFilterKegiatan = async (
@@ -108,31 +133,10 @@ export default function HalamanUtama() {
 
   const onCariPeralatan = async (e) => {
     if (e.keyCode == 13) {
-      setPage(1);
-      setKata(e.currentTarget.value);
+      setStore("page", 1);
+      setStore("cari", e.currentTarget.value);
 
-      if (searchParams.arsip) {
-        setSearchParams({
-          pagekegiatan: page(),
-        });
-      } else {
-        setSearchParams({
-          pagekegiatan: page(),
-          arsip: false,
-        });
-      }
-
-      try {
-        await onFilterKegiatan(
-          state.surreal,
-          parseInt(searchParams.pagekegiatan),
-          limit,
-          kata(),
-          searchParams.arsip ? searchParams.arsip === "true" : false
-        );
-      } catch (err) {
-        throw err;
-      }
+      refetch();
     }
   };
 
@@ -172,131 +176,147 @@ export default function HalamanUtama() {
 
   return (
     <div class="card card-compact bg-base-100 shadow-sm border">
-      <nav
-        class="flex items-center flex-column flex-wrap md:flex-row justify-between p-2"
-        aria-label="Table navigation"
-      >
-        <Show when={kosong()}>
-          <span class="ms-2 text-sm text-gray-500 dark:text-gray-300">
-            Informasi tidak ada.
-          </span>
-        </Show>
+      <Suspense fallback={<SkeletonUtama />}>
+        <Switch>
+          <Match when={kegiatan.error}>
+            <div class="card-body">
+              <div role="alert" class="alert alert-error">
+                <CircleX size={19} />
+                <span>{kegiatan.error.message}</span>
+              </div>
+            </div>
+          </Match>
 
-        <Show when={state.kegiatan.length > 0}>
-          <label class="input input-bordered input-sm flex items-center gap-2">
-            <input
-              type="text"
-              class="grow"
-              placeholder="Cari kegiatan..."
-              value={kata()}
-              onKeyUp={onCariPeralatan}
-            />
-            <Search size={19} />
-          </label>
-        </Show>
+          <Match when={kegiatan()}>
+            <nav
+              class="flex items-center flex-column flex-wrap md:flex-row justify-between p-2"
+              aria-label="Table navigation"
+            >
+              <Switch>
+                <Match when={kegiatan().length == 0}>
+                  <span class="ms-2 text-sm text-gray-500 dark:text-gray-300">
+                    Informasi tidak ada.
+                  </span>
+                </Match>
 
-        <Show when={!kosong()}>
-          <ul class="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
-            <Show when={sebelumnya()}>
-              <li>
-                <button
-                  type="button"
-                  class="btn btn-sm"
-                  classList={{
-                    "rounded-e-none": state.kegiatan.length == limit,
-                  }}
-                  onClick={prevPage}
-                >
-                  <ChevronLeft size={19} />
-                </button>
-              </li>
-            </Show>
+                <Match when={kegiatan().length > 0}>
+                  <label class="input input-bordered input-sm flex items-center gap-2">
+                    <input
+                      type="text"
+                      class="grow"
+                      placeholder="Cari kegiatan..."
+                      value={store.cari}
+                      onKeyUp={onCariPeralatan}
+                    />
+                    <Search size={19} />
+                  </label>
 
-            <Show when={selanjutnya()}>
-              <li>
-                <button
-                  type="button"
-                  class="btn btn-sm"
-                  classList={{
-                    "rounded-s-none": parseInt(searchParams.pagekegiatan) > 1,
-                  }}
-                  onClick={nextPage}
-                >
-                  <ChevronRight size={19} />
-                </button>
-              </li>
-            </Show>
-          </ul>
-        </Show>
-      </nav>
+                  <ul class="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
+                    <Show when={sebelumnya()}>
+                      <li>
+                        <button
+                          type="button"
+                          class="btn btn-sm"
+                          classList={{
+                            "rounded-e-none": state.kegiatan.length == limit,
+                          }}
+                          onClick={prevPage}
+                        >
+                          <ChevronLeft size={19} />
+                        </button>
+                      </li>
+                    </Show>
 
-      <div class="overflow-x-auto">
-        <table class="table table-zebra">
-          <thead>
-            <tr>
-              <th scope="col">Peralatan</th>
-              <th scope="col">Instansi</th>
-              <th scope="col">Status</th>
-              <th scope="col">Dibuat</th>
-              <th scope="col">Opsi</th>
-            </tr>
-          </thead>
-          <tbody>
-            <Switch>
-              <Match when={!kosong()}>
-                <For each={state.kegiatan}>
-                  {(item) => {
-                    return (
+                    <Show when={selanjutnya()}>
+                      <li>
+                        <button
+                          type="button"
+                          class="btn btn-sm"
+                          classList={{
+                            "rounded-s-none":
+                              parseInt(searchParams.pagekegiatan) > 1,
+                          }}
+                          onClick={nextPage}
+                        >
+                          <ChevronRight size={19} />
+                        </button>
+                      </li>
+                    </Show>
+                  </ul>
+                </Match>
+              </Switch>
+            </nav>
+
+            <div class="overflow-x-auto">
+              <table class="table table-zebra">
+                <thead>
+                  <tr>
+                    <th scope="col">Peralatan</th>
+                    <th scope="col">Instansi</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Dibuat</th>
+                    <th scope="col">Opsi</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <Switch>
+                    <Match when={kegiatan().length > 0}>
+                      <For each={kegiatan()}>
+                        {(item) => {
+                          return (
+                            <tr>
+                              <th scope="row">{item.peralatan}</th>
+                              <td>{item.instansi}</td>
+                              <td>
+                                <span class="badge badge-accent badge-sm">
+                                  {item.arsip ? "arsip" : "aktif"}
+                                </span>
+                              </td>
+                              <td>
+                                {item.dibuat.toLocaleString("id-id", {
+                                  weekday: "long",
+                                  year: "numeric",
+                                  month: "long",
+                                  day: "numeric",
+                                  hour: "2-digit",
+                                  minute: "2-digit",
+                                })}
+                              </td>
+                              <td>
+                                <A
+                                  href={`${item.id.id}/rincian?peralatan=${
+                                    item.peralatan
+                                  }&instansi=${item.instansi}&kalibrasi=${
+                                    item.kalibrasi
+                                  }&psatuan=${item.psatuan || "Pa"}&vsatuan=${
+                                    item.vsatuan || "m/s"
+                                  }&arsip=${item.arsip}`}
+                                  class="btn btn-ghost btn-sm"
+                                >
+                                  <EllipsisVertical size={19} />
+                                </A>
+                              </td>
+                            </tr>
+                          );
+                        }}
+                      </For>
+                    </Match>
+
+                    <Match when={kegiatan().length == 0}>
                       <tr>
-                        <th scope="row">{item.peralatan}</th>
-                        <td>{item.instansi}</td>
-                        <td>
-                          <span class="badge badge-accent badge-sm">
-                            {item.arsip ? "arsip" : "aktif"}
-                          </span>
-                        </td>
-                        <td>
-                          {item.dibuat.toLocaleString("id-id", {
-                            weekday: "long",
-                            year: "numeric",
-                            month: "long",
-                            day: "numeric",
-                            hour: "2-digit",
-                            minute: "2-digit",
-                          })}
-                        </td>
-                        <td>
-                          <A
-                            href={`${item.id.id}/rincian?peralatan=${
-                              item.peralatan
-                            }&instansi=${item.instansi}&kalibrasi=${
-                              item.kalibrasi
-                            }&psatuan=${item.psatuan || "Pa"}&vsatuan=${
-                              item.vsatuan || "m/s"
-                            }&arsip=${item.arsip}`}
-                            class="btn btn-ghost btn-sm"
-                          >
-                            <EllipsisVertical size={19} />
-                          </A>
-                        </td>
+                        <td scope="row">-</td>
+                        <td class="px-6 py-4">-</td>
+                        <td class="px-6 py-4">-</td>
+                        <td class="px-6 py-4">-</td>
                       </tr>
-                    );
-                  }}
-                </For>
-              </Match>
-
-              <Match when={kosong()}>
-                <tr>
-                  <th scope="row">-</th>
-                  <td class="px-6 py-4">-</td>
-                  <td class="px-6 py-4">-</td>
-                  <td class="px-6 py-4">-</td>
-                </tr>
-              </Match>
-            </Switch>
-          </tbody>
-        </table>
-      </div>
+                    </Match>
+                  </Switch>
+                </tbody>
+              </table>
+            </div>
+          </Match>
+        </Switch>
+      </Suspense>
     </div>
   );
 }
