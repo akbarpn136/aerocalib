8a4b2ce156a288350a161f43d25a42819032dc4d
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-10-14 22:23:31 +0700

    Penambahan padding skeleton

--- src/components/kegiatan/skeleton.jsx
+++ src/components/kegiatan/skeleton.jsx
@@ -1,6 +1,6 @@
 export default function DefaultSkeletonComponent() {
   return (
-    <div class="flex flex-col gap-4">
+    <div class="flex flex-col gap-4 p-2">
       <div class="skeleton h-32 w-full"></div>
       <div class="skeleton h-4 w-5/12"></div>
       <div class="skeleton h-4 w-5/6"></div>
